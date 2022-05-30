069a32cfd441d26ea603e8eba5c8a2d1be9faf43
SGlinkau <97282786+SGlinkau@users.noreply.github.com>
2022-05-30 18:23:56 +0200

    Test

---
+++ src/js/01-gallery.js
@@ -0,0 +1,27 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+// Opisany w dokumentacji
+import SimpleLightbox from 'simplelightbox';
+// Dodatkowy import stylów
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+const markup = galleryItems
+  .map(
+    image =>
+      `<a class="gallery__item" href="${image.original}"> <img class= "gallery__image" src="${image.preview}" 
+ alt="${image.description}"></a>`
+  )
+  .join('');
+gallery.insertAdjacentHTML('beforeend', markup);
+
+console.log(galleryItems);
+
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+
